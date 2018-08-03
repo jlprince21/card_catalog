62055866bb68ea7c7f468315819fad699c43cbf9
Luke Prince <luke@theprinces.org>
2018-08-03 15:22:33 -0400

    Initial commit of blank console app starter.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace file_scanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
