9af6ea490439c8e79935b886e29ad391fbef714c
Luke Prince <luke@theprinces.org>
2018-09-08 18:08:23 -0400

    Add config file reading.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate config;
+
 fn main() {
-    println!("Hello, world!");
+    let mut settings = config::Config::default();
+
+    settings
+        .merge(config::File::with_name("settings"))
+        .expect("Config file missing!");
+
+    // DirectoryToScan = "XXX"
+    // SQLDatabase = "XXX"
+    // SQLPassword = "XXX"
+    // SQLPort = "3306"
+    // SQLServer = "localhost"
+    // SQLUser = "user"
+
+    let testread = settings.get_str("DirectoryToScan");
+
+    println!("{:?}", settings.get_str("SQLPort"));
+    println!("{:?}", &testread);
 }
