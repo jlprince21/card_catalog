8b26003c46ee5c2a5a5c109dd124671f31dc39a8
Luke Prince <luke@theprinces.org>
2018-09-09 09:57:52 -0400

    Add working MySQL query example.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 extern crate config;
+extern crate mysql;
+
+use mysql as my;
+
+// derive line enables easy printing of struct
+#[derive(Debug)]
+struct Listing {
+    file_name: String
+}
 
 fn main() {
     let mut settings = config::Config::default();
@@ -7,15 +16,31 @@ fn main() {
         .merge(config::File::with_name("settings"))
         .expect("Config file missing!");
 
-    // DirectoryToScan = "XXX"
-    // SQLDatabase = "XXX"
-    // SQLPassword = "XXX"
-    // SQLPort = "3306"
-    // SQLServer = "localhost"
-    // SQLUser = "user"
+    let sql_user = format!("{}", settings.get_str("SQLUser").unwrap());
+    let sql_password = format!("{}", settings.get_str("SQLPassword").unwrap());
+    let sql_server = format!("{}", settings.get_str("SQLServer").unwrap());
+    let sql_port = format!("{}", settings.get_str("SQLPort").unwrap());
+    let sql_database = format!("{}", settings.get_str("SQLDatabase").unwrap());
 
-    let testread = settings.get_str("DirectoryToScan");
+    let connection_string = format!("mysql://{}:{}@{}:{}/{}", sql_user, sql_password, sql_server, sql_port, sql_database);
+    println!("{}", connection_string);
 
-    println!("{:?}", settings.get_str("SQLPort"));
-    println!("{:?}", &testread);
+    let pool = my::Pool::new(connection_string).unwrap();
+
+    let selected_listings: Vec<Listing> =
+    pool.prep_exec("SELECT FileName from Listings limit 10", ())
+    .map(|result| { // In this closure we will map `QueryResult` to `Vec<Listing>`
+        // `QueryResult` is iterator over `MyResult<row, err>` so first call to `map`
+        // will map each `MyResult` to contained `row` (no proper error handling)
+        // and second call to `map` will map each `row` to `Listing`
+        result.map(|x| x.unwrap()).map(|row| {
+            // ⚠️ Note that from_row will panic if you don't follow your schema
+            let FileName = my::from_row(row);
+            Listing {
+                file_name: FileName,
+            }
+        }).collect() // Collect Listings so now `QueryResult` is mapped to `Vec<Listing>`
+    }).unwrap(); // Unwrap `Vec<Listing>`
+
+    println!("{:?}", selected_listings);
 }
